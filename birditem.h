c1cd597a97d49aeaa318deb31dc86d1028d85e1a
yaarya <157578853+yaarya@users.noreply.github.com>
2025-04-30 18:58:36 +0530

    Add files via upload

---
+++ birditem.h
@@ -0,0 +1,55 @@
+#ifndef BIRDITEM_H
+#define BIRDITEM_H
+
+#include <QObject>
+#include <QGraphicsPixmapItem>
+#include <QPropertyAnimation>
+
+class BirdItem : public QObject, public QGraphicsPixmapItem
+{
+    Q_OBJECT
+    Q_PROPERTY(qreal rotation READ rotation WRITE setRotation)
+    Q_PROPERTY(qreal y READ y WRITE setY)
+public:
+    explicit BirdItem(QPixmap pixmap);
+
+    qreal rotation() const;
+
+    qreal y() const;
+
+    void shootUp();
+
+    void startFlying();
+
+    void freezeInPlace();
+
+
+signals:
+
+
+public slots:
+    void setRotation(qreal newRotation);
+    void setY(qreal newY);
+
+    void rotateTo(const qreal &end, const int& duration, const QEasingCurve& curve);
+
+    void fallToGroundIfNecessary();
+
+private:
+    enum WingPosition{
+        Up,
+        Middle,
+        Down
+    };
+    void updatePixmap();
+
+    WingPosition wingPosition;
+    bool wingDirection; // 0:down 1:up
+    qreal m_rotation;
+    qreal m_y;
+    QPropertyAnimation * yAnimation;
+    QPropertyAnimation * rotationAnimation;
+    qreal groundPositon;
+};
+
+#endif // BIRDITEM_H
---
+++ scene.cpp
@@ -0,0 +1,153 @@
+#include "scene.h"
+#include <QGraphicsSceneMouseEvent>
+#include <QKeyEvent>
+#include <QDebug>
+
+Scene::Scene(QObject *parent) : QGraphicsScene(parent), gameOn(false), score(0), bestScore(0), gameOverPix(nullptr), scoreTextItem(nullptr)
+{
+    setUpPillarTimer();
+
+}
+
+void Scene::addBird()
+{
+    bird = new BirdItem(QPixmap(":/images/bird_up.png"));
+    addItem(bird);
+}
+
+void Scene::startGame()
+{
+    score = 0;
+    // bird
+    bird -> startFlying();
+
+    // pillars
+    if(!pillarTimer -> isActive()){
+        cleanPillars();
+        setGameOn(true);
+
+        pillarTimer -> start(1000);
+    }
+
+    hideGameOverGraphics();
+}
+
+void Scene::setUpPillarTimer()
+{
+    pillarTimer = new QTimer(this);
+    connect(pillarTimer, &QTimer::timeout, [=](){
+
+        PillarItem * pillarItem = new PillarItem();
+        connect(pillarItem, &PillarItem::collideFail,[=](){
+            pillarTimer -> stop();
+            freezeBirdAndPillarsInPlace();
+            setGameOn(false);
+
+            showGameOverGraphics();
+        });
+
+        addItem(pillarItem);
+    });
+
+    // pillarTimer -> start(1000);
+}
+
+void Scene::freezeBirdAndPillarsInPlace()
+{
+    // freeze bird
+    bird -> freezeInPlace();
+
+    // freeze pillaars
+    QList<QGraphicsItem*> sceneItems = items();
+    foreach (QGraphicsItem * item, sceneItems){
+        PillarItem * pillar = dynamic_cast<PillarItem *>(item);
+        if(pillar){
+            pillar -> freezeInPlace();
+        }
+    }
+}
+
+bool Scene::getGameOn() const
+{
+    return gameOn;
+}
+
+void Scene::setGameOn(bool newGameOn)
+{
+    gameOn = newGameOn;
+}
+
+void Scene::incrementScore()
+{
+    score++;
+    if(score > bestScore){
+        bestScore = score;
+    }
+    qDebug() << "Score: "<< score << "Best Score: " << bestScore << "\n";
+}
+
+void Scene::showGameOverGraphics()
+{
+    gameOverPix = new QGraphicsPixmapItem(QPixmap(":/images/game_over.png"));
+    addItem(gameOverPix);
+    gameOverPix -> setPos(QPointF(0,0) - QPointF(gameOverPix->boundingRect().width()/2, gameOverPix->boundingRect().height()/2));
+
+    scoreTextItem = new QGraphicsTextItem();
+
+    QString htmlString = "<p> Score: " + QString::number(score) + "</p>" + "<p> Best score: " + QString::number(bestScore) + "</p>";
+
+    QFont mFont("Consolas", 30, QFont::Bold);
+    scoreTextItem -> setHtml(htmlString);
+    scoreTextItem -> setFont(mFont);
+    scoreTextItem -> setDefaultTextColor(Qt::black);
+    addItem(scoreTextItem);
+
+    scoreTextItem -> setPos(QPointF(0,0) - QPointF(scoreTextItem->boundingRect().width()/2, -gameOverPix->boundingRect().height()/2));
+}
+
+void Scene::hideGameOverGraphics()
+{
+    if(gameOverPix){
+        removeItem(gameOverPix);
+        delete gameOverPix;
+        gameOverPix = nullptr;
+    }
+
+    if(scoreTextItem){
+        removeItem(scoreTextItem);
+        delete scoreTextItem;
+        scoreTextItem = nullptr;
+    }
+}
+
+void Scene::cleanPillars()
+{
+    QList<QGraphicsItem *> sceneItems = items();
+    foreach (QGraphicsItem * item, sceneItems) {
+        PillarItem * pillar = dynamic_cast<PillarItem *>(item);
+        if(pillar){
+            removeItem(pillar);
+            delete pillar;
+        }
+    }
+}
+
+void Scene::keyPressEvent(QKeyEvent *event)
+{
+    if(event -> key() == Qt::Key_Space){
+        if(gameOn){
+            bird -> shootUp();
+        }
+    }
+    QGraphicsScene::keyPressEvent(event);
+}
+
+void Scene::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    if(event -> button() == Qt::LeftButton){
+        if(gameOn){
+            bird -> shootUp();
+        }
+    }
+    QGraphicsScene::mousePressEvent(event);
+}
